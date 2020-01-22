091515416bcf5a4865623bf7b1d65fd72382e7f9
ephemeralin <ephemeralin@gmail.com>
2020-01-22 21:13:08 +0300

    init commit

---
+++ src/main/java/com/ephemeralin/ListRssEntriesHandler.java
@@ -0,0 +1,44 @@
+package com.ephemeralin;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.ephemeralin.dal.RssEntry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ListRssEntriesHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private final Logger log = LogManager.getLogger(this.getClass());
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		try {
+//			 get all entries
+			List<RssEntry> products = new RssEntry().list();
+
+			log.info("test OK");
+
+			// send the response back
+			return ApiGatewayResponse.builder()
+					.setStatusCode(200)
+					.setObjectBody(products)
+					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+					.build();
+		} catch (Exception ex) {
+			log.info("test error");
+			log.error("Error in listing products: " + ex);
+
+			// send the error response back
+			Response responseBody = new Response("Error in listing RSS entries: ", input);
+			return ApiGatewayResponse.builder()
+					.setStatusCode(500)
+					.setObjectBody(responseBody)
+					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+					.build();
+		}
+	}
+}
---
+++ src/main/java/com/ephemeralin/dal/DynamoDBAdapter.java
@@ -0,0 +1,38 @@
+package com.ephemeralin.dal;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+
+public class DynamoDBAdapter {
+
+    private static DynamoDBAdapter db_adapter;
+    private final AmazonDynamoDB client;
+    private DynamoDBMapper mapper;
+
+    private DynamoDBAdapter() {
+        this.client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
+
+    public static DynamoDBAdapter getInstance() {
+        if (db_adapter == null)
+            db_adapter = new DynamoDBAdapter();
+
+        return db_adapter;
+    }
+
+    public DynamoDBMapper createDbMapper(DynamoDBMapperConfig mapperConfig) {
+        if (this.client != null)
+            mapper = new DynamoDBMapper(this.client, mapperConfig);
+
+        return this.mapper;
+    }
+
+    public AmazonDynamoDB getDbClient() {
+        return client;
+    }
+}
